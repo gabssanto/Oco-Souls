e536ee52ea44fd6dabf8c0c2617396786002dd2c
blackzarifa <rafaelirades@gmail.com>
2021-11-03 19:29:05 -0300

    Added Test Area and CameraFollow script

--- Assets/Parallax.cs
+++ Assets/Parallax.cs
@@ -8,11 +8,18 @@ public class Parallax : MonoBehaviour
     public GameObject cam;
     public float parallaxEffect;
 
+    private float lengthY, startposY;
+    public float parallaxEffectY;
+
     // Start is called before the first frame update
     void Start()
     {
         startpos = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
+
+
+        startposY = transform.position.y;
+        lengthY = GetComponent<SpriteRenderer>().bounds.size.y;
     }
 
     // Update is called once per frame
@@ -21,7 +28,7 @@ public class Parallax : MonoBehaviour
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
 
-        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        /*transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
 
         if (temp > startpos + length)
         {
@@ -30,6 +37,30 @@ public class Parallax : MonoBehaviour
         else if (temp < startpos - length)
         {
             startpos -= length;
+        }*/
+
+
+        float tempY = (cam.transform.position.y * (1 - parallaxEffectY));
+        float distY = (cam.transform.position.y * parallaxEffectY);
+
+        transform.position = new Vector3(startpos + dist, startposY + distY, transform.position.z);
+
+        if (temp > startpos + length)
+        {
+            startpos += length;
+        }
+        else if (temp < startpos - length)
+        {
+            startpos -= length;
+        }
+
+        if (tempY > startposY + lengthY)
+        {
+            startposY += lengthY;
+        }
+        else if (tempY < startposY - lengthY)
+        {
+            startposY -= lengthY;
         }
     }
 }
