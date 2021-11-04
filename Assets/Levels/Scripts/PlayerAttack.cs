4f38d3235ce8699295f50d027ee0af2ec3500ef3
Gabriel Santo <espiritosanto.gabriel@gmail.com>
2021-11-03 23:27:27 -0300

    added animations for boss

--- Assets/Levels/Scripts/PlayerAttack.cs
+++ Assets/Levels/Scripts/PlayerAttack.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    [SerializeField] private float attackCooldown;
+    private Animator animator;
+    private PlayerMovement playerMovement;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
