21430cc58274e25a32e50af550ce4b12294ce973
blackzarifa <rafaelirades@gmail.com>
2021-11-19 10:26:17 -0300

    Finished setting menu

--- Assets/Levels/Scripts/Menu/SettingsMenu.cs
+++ Assets/Levels/Scripts/Menu/SettingsMenu.cs
@@ -19,18 +19,34 @@ public class SettingsMenu : MonoBehaviour
 
         List<string> options = new List<string>();
 
+        int currentResolutionindex = 0;
+
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
+
+            if (resolutions[i].width == Screen.width && 
+                resolutions[i].height == Screen.height)
+            {
+                currentResolutionindex = i;
+            }
         }
 
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionindex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
     public void SetVolume (float volume)
     {
-        audioMixer.SetFloat("mainVolume", volume);
+        audioMixer.SetFloat("mainVolume", Mathf.Log10(volume) * 20);
     }
 
 
