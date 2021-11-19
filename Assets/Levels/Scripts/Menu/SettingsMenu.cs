010294e8ab4ff3b49f5ff3368e08d8420f40b152
blackzarifa <rafaelirades@gmail.com>
2021-11-19 01:02:28 -0300

    Added more options to the settings menu, edited collision box for spikes on MainLevel

---
+++ Assets/Levels/Scripts/Menu/SettingsMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using TMPro;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    private void Start ()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+        }
+
+        resolutionDropdown.AddOptions(options);
+    }
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("mainVolume", volume);
+    }
+
+
+}
