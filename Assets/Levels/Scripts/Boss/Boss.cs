557e9183317c09f6366144a3e42ebac83fd4a3a3
blackzarifa <rafaelirades@gmail.com>
2021-11-26 21:23:56 -0300

    Fixed Boss walk animation, made a few changes to big skeleton

---
+++ Assets/Levels/Scripts/Boss/Boss.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] bool isFlipped = false;
+
+    public void LookAtPlayer()
+    {
+        Vector3 flipped = transform.localScale;
+        flipped.z *= -1f;
+
+        if (transform.position.x > player.position.x && isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+
+            isFlipped = false;
+        }
+        else if (transform.position.x < player.position.x && !isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+
+            isFlipped = true;
+        }
+    }
+}
--- Assets/Levels/Scripts/Boss/BossWalk.cs
+++ Assets/Levels/Scripts/Boss/BossWalk.cs
@@ -8,21 +8,23 @@ public class BossWalk : StateMachineBehaviour
 
     Transform player;
     Rigidbody2D rb;
+    Boss boss;
 
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         rb = animator.GetComponent<Rigidbody2D>();
+        boss = animator.GetComponent<Boss>();
     }
 
     //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Vector2 target = new Vector2(player.position.x, rb.position.y);
-        Vector2 newPos = Vector2.MoveTowards(rb.position, target, 2500f);
+        boss.LookAtPlayer();
 
-        Debug.Log("newPos: " + newPos + "\trb: " + rb.position + "\ttarget: " + target);
+        Vector2 target = new Vector2(player.position.x, rb.position.y);
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
         
         rb.MovePosition(newPos);
     }
